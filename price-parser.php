da1760c50e4f79c44430b4289721f39ec874d887
Oleh Sidun <sidunoleh@gmail.com>
2024-02-17 01:38:08 +0200

    schedule parse price event

--- price-parser.php
+++ price-parser.php
@@ -6,6 +6,8 @@
  * Author: Sidun Oleh
  */
 
+use PriceParser\Task;
+
 defined('ABSPATH') or die;
 
 /**
@@ -17,3 +19,23 @@ const PRICE_PARSER_ROOT = __DIR__;
  * Composer autoloader
  */
 require_once PRICE_PARSER_ROOT . '/vendor/autoload.php';
+
+/**
+ * Schedule parse price event
+ */
+function schedule_parse_price_event() {
+    if (! wp_next_scheduled('parse_price_event')) {
+        wp_schedule_event(
+            time(), 
+            'daily', 
+            'parse_price_event'
+        );
+    }
+}
+
+register_activation_hook(__FILE__, 'schedule_parse_price_event');
+
+/**
+ * Parse price
+ */
+add_action('parse_price_event', new Task);
