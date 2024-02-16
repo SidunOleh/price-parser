de0d56ea5905d73e353c8a134006f5684aa36842
Oleh Sidun <sidunoleh@gmail.com>
2024-02-17 00:28:52 +0200

    ini

---
+++ price-parser.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Price parser
+ * Description: Parse products prices from www.precisionzone.net
+ * Author: Sidun Oleh
+ */
+
+defined('ABSPATH') or die;
+
+/**
+ * Plugin root
+ */
+const PRICE_PARSER_ROOT = __DIR__;
+
+/**
+ * Composer autoloader
+ */
+require_once IMPORT_ROOT . '/vendor/autoload.php';
